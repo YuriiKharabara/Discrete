79f0cf15b0949623d98884999c04aa8bc25e1eb9
YuriiKhr <yurii.kharabara@ucu.edu.ua>
2021-12-18 14:33:46 +0200

    something not finished

--- try.py
+++ try.py
@@ -1,35 +1,29 @@
-def dfs(graph, start, visited=None, parent=[], articulation_points=set()):
+# def dfs(graph, start, visited=None, parent=[], articulation_points=set()):
+#     if visited is None:
+#         visited = set()
+#     visited.add(start)
+#     print(start)
+#     for next in graph[start] - visited:
+#         dfs(graph, next, visited, articulation_points)
+#     return visited
+def dfs(graph, start, visited=None):
     if visited is None:
-        visited = set()
+        visited = set()     
     visited.add(start)
     print(start)
     for next in graph[start] - visited:
-        dfs(graph, next, visited, articulation_points)
+        dfs(graph, next, visited)
     return visited
-
-
-def dfs_iterative(graph, start):
-    stack=[start]
-    stack= [start]
-    path=[]
-
-    while stack:
-        vertex = stack.pop()
-        if vertex in path:
-            continue
-        path.append(vertex)
-        for neighbor in graph[vertex]:
-            stack.append(neighbor)
-
-    return path
+ 
 
 
 
-
-graph = {'0': set(['1', '2']),
-         '1': set(['0', '3', '4']),
-         '2': set(['0']),
-         '3': set(['1']),
-         '4': set(['2', '3'])}
-dfs(graph, '0')
-print(dfs_iterative(graph, '0'))
+# graph = {'0': set(['1', '2']),
+#          '1': set(['0', '3', '4']),
+#          '2': set(['0']),
+#          '3': set(['1']),
+#          '4': set(['2', '3'])}
+# graph = {1: [2], 2: [1], 3: [4], 4: [3], 6: [7], 7: [6]}
+graph={1: set([2, 5]), 2: set([3,4,1]), 3: set([2]), 4: set([2]), 5: set([1,6,7]), 6: set([5]), 7: set([5])}
+print(graph)
+print(dfs(graph, 1))
