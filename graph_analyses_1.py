bcf8d9552246101f19956ffa78087680c9e0815c
Viktoria Kocherkevych <v.kocherkevych@ucu.edu.ua>
2021-12-19 22:27:26 +0200

    Тут з'явився вивід для 3, 4, 5 функцій

--- graph_analyses_1.py
+++ graph_analyses_1.py
@@ -1,25 +1,47 @@
-path_to_file = input("Введіть шлях до файлу:")
-print("Чи орієнтований граф(0), чи ні(1)?")
-type = input("(0/1): ")
-def read_graph(path_to_file, type):
+import csv
+# path_to_file = input("Введіть шлях до файлу:")
+# print("Чи орієнтований граф(0), чи ні(1)?")
+# type = input("(0/1): ")
+def read_graph(path_to_file, type, project):
     """
     :param path_to_file:
     :param type: if type == 0, graph is oriented, if type == 1, graph is not oriented 
     :return: set of tuples
     """
+    four={}
+    three=set()
+    five=[]
     final_set=set()
-    new_set = set()
+    three_2={}
     with open(path_to_file, 'r') as file:
         for line in file:
             line = line.strip()
-            one_line = tuple(line.split(" "))
-            final_set.add(one_line)
-            # final_set = {(2,1), (1,4), (3,2), (3,1), (3,4), (3,5), (4,5)}
+            line = line.split(" ")
+            first = line[0]
+            sec = line[1]
+            if first in four.keys():
+                four[first].add(sec)
+                three_2[first].append(sec)
+            else:
+                four[first] = set(sec)
+                three_2[first] = [sec]
             if int(type) == 1:
-                one_line = list(one_line)
-                if one_line[0] != one_line[1]:
-                    new = tuple([one_line[1], one_line[0]])
-                    new_set.add(new)
-        final_set.update(new_set)
-# final_set = {(1, 3), (1, 2), (3, 4), (2, 1), (4, 3), (3, 1), (5, 4), (1, 4), (2, 3), (4, 5), (5, 3), (3, 2), (4, 1), (3, 5)}
-    return final_set
+                if sec in four.keys():
+                    four[sec].add(first)
+                    three_2[sec].append(first)
+                else:
+                    four[sec] = set(first)
+                    three_2[sec] = [first]
+            final_set.add((int(first), int(sec)))
+            if project == 3:
+                three.add(int(first))
+                three.add(int(sec))
+            five=[three_2[value] for value in sorted(three_2)]
+    if project == 3:
+        # return three_2
+        three = sorted(list(three))
+        return (final_set, three)
+    elif project == 4:
+        return four
+    elif project == 5:
+        return five
