348d0baf07ce28382fe228fcf86a2e09f1d7aa70
Svitlana Hovorova <cybermavka@Svitlanas-MacBook-Air.local>
2021-12-20 20:52:18 +0200

    aaaa

--- scc_ver1_fin.py
+++ scc_ver1_fin.py
@@ -1,4 +1,25 @@
 from collections import defaultdict
+def reading_graph(path_to_file, type):
+    """
+    :param path_to_file:
+    :param type: if type == 0, graph is oriented, if type == 1, graph is not oriented
+    :return:
+    """
+    pre_final_set = set()
+    final_set = set()
+    with open(path_to_file, 'r') as file:
+        first = True
+        for line in file:
+            line = line.strip()
+            if first:
+                pre_final_set.add((int(line.split(" ")[0]), int(line.split(" ")[1])))
+                first = False
+            else:
+                one_line = (int(line.split(" ")[0]), int(line.split(" ")[1]))
+                final_set.add(one_line)
+    #print(pre_final_set)
+    # return list(pre_final_set), final_set
+    return list(pre_final_set), final_set
 
 def add_edge(graph, s, d):
     graph[s].add(d)
@@ -64,4 +85,12 @@ add_edge(g, 6, 4)
 
 #print(g)
 #print("Strongly Connected Components:")
+#print(kosaraju_scc(g, 8))
+
+# data = reading_graph('graph_100_1942_0.csv', 1)
+# n = data[0][0][1]
+
+# for i in data[1]:
+#     add_edge(g, i[0], i[1])
+
 print(kosaraju_scc(g, 8))
